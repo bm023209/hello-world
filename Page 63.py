a372bb7e4350aab830e3020add1c3b6016df8a1c
bm023209 <michael.berghoff@devoteam.com>
2017-01-03 12:31:58 +0100

    Add files via upload

---
+++ Page 63.py
@@ -0,0 +1,12 @@
+print ('Enter Password#')
+password= input ()
+print ('Enter Name')
+name=input()
+if name=='mary':
+	print ('hello mary')
+	if password == 'secret':
+		print ('Access granded')
+	else:
+		print ('Access denide')
+else:
+        print (name +' wrong name')
